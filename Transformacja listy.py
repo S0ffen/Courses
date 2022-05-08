70d06e7b8d9905e4a510de2fed85428738fc07bb
ja <whitevolf314@gmail.com>
2022-05-08 18:49:44 +0200

    changes

--- Transformacja listy.py
+++ Transformacja listy.py
@@ -1,5 +1,7 @@
 liczby = [1,2,3,4,5,6]
-
+'''
+Sposób Pierwszy
+'''
 potegiDwojki = []
 for element in liczby:
     potegiDwojki.append(element**2)
@@ -9,5 +11,19 @@ for element in liczby:
     if (element % 2) == 0:
         liczbyParzyste.append(element)
 
-print(potegiDwojki)
-print(liczbyParzyste)
+#print(potegiDwojki)
+#print(liczbyParzyste)
+
+'''
+Sposób drugi (działa szybciej)
+'''
+
+potegiDwojki2 = [element**2 
+                for element in liczby]
+#print(potegiDwojki2)
+
+liczbyParzyste2 = [element
+                    for element in liczby
+                    if element % 2 == 0]
+
+print(liczbyParzyste2)
