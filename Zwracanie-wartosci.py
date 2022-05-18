1b2121b409e91ad43a32ebe7ec460b2597ecb474
ja <whitevolf314@gmail.com>
2022-05-18 16:49:27 +0200

    Lesson 47

--- Zwracanie-wartosci.py
+++ Zwracanie-wartosci.py
@@ -1,4 +1,5 @@
-""" return - zwrócić, pozwala na modyfikacje zwróconej wartości,  None - nic - false, return kończy funkcje tak samo jak break"""
+""" return - zwrócić, pozwala na modyfikacje zwróconej wartości,  None - nic - false, return kończy funkcje tak samo jak break
+"""
 
 def pole_prostokata(a, b):
     return a * b
@@ -20,3 +21,20 @@ if (x):
     print("Podzielono poprawnie", x)
 else:
     print("Coś poszło nie tak")
+
+"""
+Notatki z lekcji 46
+Słowo return jak sama nazwa wskazuje z ang. powraca do miejsca wywołania.
+
+Miejsce wywołania jest decydowane przez Ciebie.
+
+Jeśli napiszesz w kodzie:
+
+poteguj(2, 3);
+
+to miejsce, w którym to napiszesz, jest miejscem gdzie ta funkcja zostaje wywołana.
+
+Po tym gdy wywołasz funkcję komputer przeskakuje do deklaracji funkcji (def). Następnie komputer wykonuje wszystkie instrukcje wewnątrz funkcji aż do momentu napotkania "return", czyli powrotu do miejsca wywołania. Miejsce, gdzie została funkcja wywołana jest zastępowane przez to co jest po słówku return.
+
+Wywołanie oznacza, wykonanie/przeskoczenie do funkcji i jej rozpoczęcie.
+"""
