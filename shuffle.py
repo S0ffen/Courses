b546d1f259337d712b12f180043af3e904babf12
ja <whitevolf314@gmail.com>
2022-05-26 21:14:09 +0200

    Lesson 64 Command Shuffle

---
+++ shuffle.py
@@ -0,0 +1,23 @@
+"""
+shuffle - zmnienia kolejność elementów w liście
+            (Działa na oryginale)
+
+"""
+
+import random
+
+cardList = ["9", "9", "9", "9",
+            "10", "10", "10", "10",
+            "Jack", "Jack", "Jack", "Jack",
+            "Queen", "Queen", "Queen", "Queen",
+            "King", "King", "King", "King",
+            "Ace", "Ace", "Ace", "Ace"
+            "Joker", "Joker"]
+
+
+random.shuffle(cardList)
+
+print(cardList)
+
+
+
