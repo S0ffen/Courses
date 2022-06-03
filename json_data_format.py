c4260bd9adaf4e73f987ee3b5b0a2832b5ba463a
ja <whitevolf314@gmail.com>
2022-06-04 00:03:54 +0200

    Lesson 77 What is JSON

---
+++ json_data_format.py
@@ -0,0 +1,45 @@
+"""
+
+JSON
+
+"""
+
+"""
+Zapis w Pythonie
+"""
+
+film = {
+    "title" : "Ale ja nie będę tego robił!",
+    "release_year" : 1969,
+    "won_oscar" : True,
+    "actors": ("Arkadiusz Włodarczyk", "Wiolleta Włodarczyk"),
+    "budget" : None,
+    "credits" : {
+            "director" : "Arkadiusz Włodarczyk",
+            "writer" : "Alan Burger",
+            "animator" : "Anime Animatrix"
+            }
+}
+
+
+
+"""
+Zapis w Jsonie
+
+{  
+   "title":"Ale ja nie będę tego robił!",
+   "release_year":1969,
+   "won_oscar":true,
+   "actors":[  
+      "Arkadiusz Włodarczyk",
+      "Wiolleta Włodarczyk"
+   ],
+   "budget":null,
+   "credits":{  
+      "director":"Arkadiusz Włodarczyk",
+      "writer":"Alan Burger",
+      "animator":"Anime Animatrix"
+   }
+}
+"""
+
