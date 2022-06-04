f91846339d8392ef93477ac40110d86a0710e6d0
ja <whitevolf314@gmail.com>
2022-06-04 18:31:14 +0200

    Lesson 79 How to open json file in Python

--- Jak_Otworzyć_Plik_Json_W_Python.py
+++ Jak_Otworzyć_Plik_Json_W_Python.py
@@ -24,7 +24,7 @@ film = {
 encodedRetrivedMovie = '{"title": "Ale ja nie będę tego robił!", "release_year": 1969, "won_oscar": true, "actors": ["Arkadiusz Włodarczyk", "Wiolleta Włodarczyk"], "budget": null, "credits": {"director": "Arkadiusz Włodarczyk", "writer": "Alan Burger", "animator": "Anime Animatrix"}}'
 decodedMovie = json.loads(encodedRetrivedMovie)
 
-print(decodedMovie)
+#print(decodedMovie)
 
 with open ("sample.json",encoding="UTF-8") as file:
     wynik = json.load(file)
