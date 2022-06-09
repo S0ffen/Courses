d43bcaec588cd2bd6fa2b380a67dd6cccc127c26
ja <whitevolf314@gmail.com>
2022-06-09 21:23:57 +0200

    Lesson 89,90,91,92 How API works, and some useful libraries

--- public_API.py
+++ public_API.py
@@ -1,28 +1,39 @@
 import pprint
-
+import webbrowser
 import requests
 import json
+from datetime import datetime, timedelta
 
 """
 
 API - Application Programming Interface
 
+timestamp - znak czasu 
+od 1 stycznia 1970 w sekundach 
+
 """
+
+timeBefore = timedelta(days=10)
+
+searchDate = datetime.today() - timeBefore
+
 
 params = {
     "site": "stackoverflow",
     "sort": "votes",
     "order": "desc",
-    "fromdate": "2022-05-15",
+    "fromdate": int(searchDate.timestamp()),
     "tagged": "python",
     "min": 15,
 }
 
 r = requests.get('https://api.stackexchange.com/2.3/questions/', params)
 
+
 try:
     questions = r.json()
 except json.JSONDecodeError:
     print("Nie poprawny format")
 else:
-    pprint.pprint(questions)
+    for questions in questions["items"]:
+        webbrowser.open_new_tab(questions['link'])
